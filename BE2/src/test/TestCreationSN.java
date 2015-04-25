4d89ce1e6b0e043da687398950c8e2e2bbdf35c8
JordanMl <jordan.souls@gmail.com>
2015-04-25 23:40:29 +0200

    Update TestCreationSN.java

--- BE2/src/test/TestCreationSN.java
+++ BE2/src/test/TestCreationSN.java
@@ -17,50 +17,21 @@ public class TestCreationSN {
 	public static SocialNetwork createNewSocialNetwork() throws BadEntry, MemberAlreadyExists, NotMember, ItemBookAlreadyExists, ItemFilmAlreadyExists{
 		
 		SocialNetwork sn = new SocialNetwork();
-		//Ajout de 3 membres au réseau social
-		try{
+		//Ajout de 3 membres au réseau social		
 			sn.addMember("Personne1","psw1","haha");
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}		
-		try{
 			sn.addMember("Personne2","psw2","hoho");
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		try{
-			sn.addMember("Personne3","psw3","hihi");
-		}
-		catch(BadEntry e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
+			sn.addMember("Personne3","psw3","hihi");		
 		
-		
-		//Ajout de 3 livres au réseau social
-		try{
+		//Ajout de 3 livres au réseau social		
 			sn.addItemBook("Personne1", "psw1", "Arts martiaux", "Documentaire", "Jackie Chan", 150);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		try{
 			sn.addItemBook("Personne2", "psw2", "La Chine", "Histoire", "Yang Ping Pong", 328);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		try{
 			sn.addItemBook("Personne3", "psw3", "Coupe du monde football", "Sport", "Zinedine Zidane", 12);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		
-		
-		//Ajout de 3 films au réseau social
-		try{
-			sn.addItemFilm("Personne1", "psw1", "Interstellar", "Science-fiction", "Christopher Nolan", "Christopher Nolan", 169);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		try{
-			sn.addItemFilm("Personne2", "psw2", "Matrix", "Action", "Andy Wachowski", "Lana Wachowski", 130);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
-		try{
-			sn.addItemFilm("Personne3", "psw3", "Titanic", "Drame", "James Cameron", "James Cameron", 194);
-		}
-		catch(Exception e){System.out.println("La création du réseau social ne s'est pas bien déroulée.");}
 				
+		//Ajout de 3 films au réseau social	
+			sn.addItemFilm("Personne1", "psw1", "Interstellar", "Science-fiction", "Christopher Nolan", "Christopher Nolan", 169);
+			sn.addItemFilm("Personne2", "psw2", "Matrix", "Action", "Andy Wachowski", "Lana Wachowski", 130);
+			sn.addItemFilm("Personne3", "psw3", "Titanic", "Drame", "James Cameron", "James Cameron", 194);
+			
 		return sn; //Retourne le réseau social créé
 		
 	}
