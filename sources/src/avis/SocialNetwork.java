323b5f1a86ebaa1cb46b3c9bcb5d180834b57b31
Tezonis <mlavocat@gmail.com>
2015-05-19 16:18:26 +0200

    RE deletePreviousMemberReview

--- sources/src/avis/SocialNetwork.java
+++ sources/src/avis/SocialNetwork.java
@@ -354,7 +354,7 @@ public class SocialNetwork {
 		float tempAverageRating = 0.0f;
 		for (Item itemfilm : items){
 			if (itemfilm.getTitre().trim().toLowerCase().equals(titre.trim().toLowerCase()) && itemfilm instanceof ItemFilm){
-				itemfilm.addReviewToItem(note, commentaire);
+				itemfilm.addReviewToItem(note, commentaire, pseudo);
 				itemfilm.averageRating();
 				tempAverageRating = itemfilm.getAverageRating();
 			}
@@ -422,7 +422,8 @@ public class SocialNetwork {
 		float tempAverageRating = 0.0f;
 		for (Item itembook : items){
 			if (itembook.getTitre().trim().toLowerCase().equals(titre.trim().toLowerCase()) && itembook instanceof ItemBook){
-				itembook.addReviewToItem(note, commentaire);
+				itembook.deletePreviousMemberReview(pseudo);
+				itembook.addReviewToItem(note, commentaire, pseudo);
 				itembook.averageRating();
 				tempAverageRating = itembook.getAverageRating();
 			}
