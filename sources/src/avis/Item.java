a9dd3083f385f38c943b5b1ec5a0f29e5687769b
Tezonis <mlavocat@gmail.com>
2015-05-21 22:30:07 +0200

    Javadoc Item/Review

--- sources/src/avis/Item.java
+++ sources/src/avis/Item.java
@@ -19,6 +19,7 @@ public abstract class Item {
 		
 	/**
 	 * Met a jour la note de l'item (calcul de la moyenne des différents commentaires)
+	 * 
 	 * @return
 	 */	
 	public void averageRating(){
@@ -26,76 +27,98 @@ public abstract class Item {
 		float rateSum = 0;
 		float coefSum = 0;
 		for (Review review : reviews){
-			rateSum += review.getNote()*review.getCoef(); //somme des notes pondérées (chacunes multipliées par leur coefs) données à un item
+			rateSum += review.getNote()*review.getCoef(); //somme des notes pondérées (chacunes multipliées par leur coef) données à un item
 			coefSum += review.getCoef(); //somme des coefs
 		}
 		averageRating = rateSum/coefSum; //Calcul de la moyenne
 	}
 	
 	/**
+	 * Ajoute un nouvel avis à la liste des avis de l'item
 	 * 
-	 * 
-	 */	
+	 * @param note note choisie par le membre
+	 * @param coef poids que la note aura dans le calcul de la moyenne (correspond au karma du membre)
+	 * @param commentaire le commentaire que fait le membre de l'item
+	 * @param pseudo le pseudo du membre
+	 */
 	public void addReviewToItem(float note, float coef, String commentaire, String pseudo){
 		Review newReview = new Review(note, coef, commentaire, pseudo);
 		reviews.add(newReview);
 	}
 	
 	/**
+	 * Vérifie si le membre a déjà posté un avis sur l'item et supprime cet avis si c'est le cas
 	 * 
-	 * 
-	 */	
+	 * @param pseudo pseudo du membre à vérifier
+	 */
 	public void deletePreviousMemberReview(String pseudo){
-		//Vérifie si le membre a déjà posté un avis sur cet item et supprimer cet avis si c'est le cas
 		Review tempReview = new Review(0, 0, "", "");
 		for (Review review : reviews){
-			if (review.getPseudo().trim().toLowerCase().equals(pseudo.trim().toLowerCase())) tempReview=review;
+			if (review.getPseudo().trim().toLowerCase().equals(pseudo.trim().toLowerCase())) tempReview=review; //récupération de l'avis s'il existe
 		}
-		reviews.remove(tempReview);
+		reviews.remove(tempReview); //suppression de l'avis
 	}
 	
-	/**
-	 * 
-	 * 
-	 */	
+/**
+ * Ajoute une nouvelle note à un avis de l'item
+ * 
+ * @param pseudo1 pseudo du membre qui note l'avis
+ * @param pseudo2 pseudo du membre dont l'avis est noté
+ * @param note note à ajouter
+ */
 	public void addNoteToReview(String pseudo1, String pseudo2, float note){
 		for (Review review : reviews){
 			if (review.getPseudo().trim().toLowerCase().equals(pseudo2.trim().toLowerCase())){
-				review.noteReviewUpdate(pseudo1, note);
+				review.noteReviewUpdate(pseudo1, note); //appel de la fonction qui va ajouter la note et mettre à jour la moyenne
 			}
 		}
 	}
 	
 	/**
+	 * Accesseur de la note moyenne d'un avis de l'item
 	 * 
-	 * 
-	 */	
-	public float getNoteReview(String pseudo1, String pseudo2){
-		for (Review review : reviews){
-			if (review.getPseudo().trim().toLowerCase().equals(pseudo2.trim().toLowerCase())) return review.getNoteReview();
+	 * @param pseudo1 pseudo du membre qui note l'avis
+	 * @param pseudo2 pseudo du membre dont l'avis est noté
+	 * @return la note moyenne de l'avis du membre
+	 */
+		public float getNoteReview(String pseudo1, String pseudo2){
+			for (Review review : reviews){
+				if (review.getPseudo().trim().toLowerCase().equals(pseudo2.trim().toLowerCase())) return review.getNoteReview(); //appel de l'accesseur de l'avis
+			}
+			return 0;
 		}
-		return 0;
-	}
 	
+		/**
+		 * Vérifie si le membre a bien commenté l'item
+		 * 
+		 * @param pseudo pseudo du membre
+		 * @return true si oui, false si non
+		 */
 	public boolean isMemberReview(String pseudo){
-		//Vérifie si le membre a bien commenté l'item
 		for (Review review : reviews){
-			if (review.getPseudo().trim().toLowerCase().equals(pseudo.trim().toLowerCase())) return true;
+			if (review.getPseudo().trim().toLowerCase().equals(pseudo.trim().toLowerCase())) return true; //retourne true si un avis avec le pseudo du membre est trouvé
 		}
 		return false;
 	}
 	
+	/**
+	 * Vérifie si le membre à déjà noté un avis de l'item
+	 * 
+	 * @param pseudo1 pseudo du membre à vérifier
+	 * @param pseudo2 pseudo du membre dont l'avis est noté
+	 * @return true si oui, false si non
+	 */
 	public boolean memberAlreadyReviewOpinion(String pseudo1, String pseudo2){
-		//Vérifie si le membre à déjà noté cet avis
 		for (Review review : reviews){
-			if (review.getPseudo().trim().toLowerCase().equals(pseudo2.trim().toLowerCase())) return review.memberAlreadyReviewOpinion(pseudo1);
+			if (review.getPseudo().trim().toLowerCase().equals(pseudo2.trim().toLowerCase())) return review.memberAlreadyReviewOpinion(pseudo1); //appel la fonction qui va parcourir la liste des membres ayants déjà noté l'avis
 		}
 		return false;
 	}
 	
 	/**
 	 * Accesseur du titre de l'item
-	 * @return
+	 * 
+	 * @return le titre de l'item
 	 */
 	public String getTitre(){
 		return titre;
@@ -103,13 +126,15 @@ public abstract class Item {
 	
 	/**
 	 * Accesseur de la note moyenne
-	 * @return
+	 * 
+	 * @return la note moyenne de l'item
 	 */
 	public float getAverageRating(){
 		return averageRating;
 	}
 		
 	/**
+	 * 
 	 */
 	public abstract String toString();
 	
--- sources/src/avis/Review.java
+++ sources/src/avis/Review.java
@@ -4,13 +4,21 @@ import java.util.LinkedList;
 
 public class Review {
 	
-	private float note;
-	private float noteReview;
+	private float note; //note de l'avis sur l'item (donnée par le membre)
+	private float noteReview; //note moyenne sur cet avis
 	private float coef; //poids qu'aura la note dans le calcul de la moyenne, corespond au karma du membre lorsqu'il a posté cet avis
-	private String commentaire;
-	private String pseudo;
-	private LinkedList<String> reviewOpinions; //liste pseudos des membres ayant noté cet avis
+	private String commentaire; //commentaire du membre sur l'item
+	private String pseudo; //pseudo du membre ayant posté l'avis
+	private LinkedList<String> reviewOpinions; //liste du pseudo des membres ayant noté cet avis
 	
+	/**
+	 * Constructeur de l'avis
+	 * 
+	 * @param note
+	 * @param coef
+	 * @param commentaire
+	 * @param pseudo
+	 */
 	public Review(float note, float coef, String commentaire, String pseudo){
 		this.note = note;
 		this.commentaire = commentaire;
@@ -19,42 +27,74 @@ public class Review {
 		reviewOpinions = new LinkedList<String>();
 	}
 	
+	/**
+	 * Met à jour la moyenne de l'avis avec la nouvelle note
+	 * 
+	 * @param pseudo1 pseudo du membre qui note l'avis
+	 * @param note note que le membre donne à l'avis
+	 */
 	public void noteReviewUpdate(String pseudo1, float note){
-		float temp1 = noteReview*reviewOpinions.size();
-		float temp2 = temp1+note;
-		reviewOpinions.add(pseudo1);
-		noteReview=temp2/reviewOpinions.size();
+		float temp1 = noteReview*reviewOpinions.size(); //calcul de la somme des notes précédentes
+		float temp2 = temp1+note; //ajout de la nouvelle note à cette somme
+		reviewOpinions.add(pseudo1); //ajout du pseudo du membre à la liste des membres ayants noté cet avis
+		noteReview=temp2/reviewOpinions.size(); //calcul de la nouvelle moyenne
 	}
 	
-	public boolean memberAlreadyReviewOpinion(String pseudo2){
+	/**
+	 * Vérifie si le membre à déjà noté un avis de l'item
+	 * 
+	 * @param pseudo1 pseudo du membre à vérifier
+	 * @return true si oui, false si non
+	 */
+	public boolean memberAlreadyReviewOpinion(String pseudo1){
 		for (String reviewOpinion : reviewOpinions){
-			if (reviewOpinion.trim().toLowerCase().equals(pseudo2.trim().toLowerCase())) return true;
+			if (reviewOpinion.trim().toLowerCase().equals(pseudo1.trim().toLowerCase())) return true; //retourne true si le membre est présent dans la liste 
 		}
 		return false;
 	}
 	
-	//accesseur de la note moyenne d'un avis
+	/**
+	 * Accesseur de la note moyenne d'un avis
+	 * 
+	 * @return
+	 */
 	public float getNoteReview(){
 		return noteReview;
 	}
 	
-	//accesseur de la note de l'avis sur l'item
+	/**
+	 * Accesseur de la note donnée à l'item
+	 * 
+	 * @return la note donnée à l'item
+	 */
 	public float getNote(){
 		return note;
 	}
 	
-	//accesseur du coef
+	/**
+	 * Accesseur du coefficient
+	 * 
+	 * @return le coefficient
+	 */
 	public float getCoef(){
 		return coef;
 	}
 	
-	public String toString(){
-		return (" note : "+note+ " commentaire : <<"+commentaire+">>");
-	}
-	
-	//Accesseur du pseudo 
+	/**
+	 * Accesseur du pseudo du membre ayant posté l'avis
+	 * 
+	 * @return le pseudo du membre ayant posté l'avis
+	 */
 	public String getPseudo() {
 		return pseudo;
+	}
+	
+	/**
+	 * Renvoie une chaine de caractères représentatif de l'avis
+	 * 
+	 */
+	public String toString(){
+		return (" note : "+note+ " commentaire : <<"+commentaire+">>");
 	}
 	
 }
